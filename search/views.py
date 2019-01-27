545a0e408849e5ed13bb205be1734504003398e7
Andy Lam <theandylam@gmail.com>
2019-01-27 00:51:02 -0800

    Completed results display
    Added in the ability to query the Google books API with the input from the
    user and display a list of books related to the search term.

--- search/views.py
+++ search/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from apiclient.discovery import build
+from os import environ
 from .forms import SearchForm
 
 # Create your views here.
@@ -17,4 +19,22 @@ def search(request):
     return render(request, 'search.html', {'query' : query})
 
 def results(request, search_text):
-    return render(request, 'results.html', {'search_text' : search_text})
+    #The api key is loaded in from environment vaiables
+    api_key = environ['GOOGLE_API_KEY']
+    service = build('books', 'v1', developerKey=api_key)
+    book_request = service.volumes().list(source='public', q=search_text)
+    response = book_request.execute()
+
+    books_found = response.get('totalItems', 0)
+    book_list = response.get('items', [])
+
+    query = SearchForm()
+
+    context = {
+        'search_text' : search_text,
+        'books_found' : books_found,
+        'book_list' : book_list,
+        'query' : query,
+    }
+
+    return render(request, 'results.html', context=context)
