4105295886c401a01b6a1d7f29331f50176d28b1
Andy Lam <theandylam@gmail.com>
2019-01-27 23:04:53 -0800

    Turned off debug setting

--- booksearch/settings.py
+++ booksearch/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fqvczb9wm8l2(t#7uhse&xlq8df4i^wym@3_l&$9igm8noh7^o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
