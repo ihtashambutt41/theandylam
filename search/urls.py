a0567f0cd74c7727cb6d09d365e33d67e2f5e446
Andy Lam <theandylam@gmail.com>
2019-01-27 16:51:00 -0800

    Changed results to use djangos class based views

--- search/urls.py
+++ search/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.SearchView.as_view(), name='search'),
 
     #Example URL: http://127.0.0.1:8000/search/results/harry%20potter/
-    path('results/<str:search_text>/', views.results, name='results'),
+    path('results/<str:search_text>/', views.ResultsView.as_view(), name='results'),
 ]
--- search/views.py
+++ search/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic.edit import FormView
+from django.views.generic.list import ListView
 from apiclient.discovery import build
 from os import environ
 from .forms import SearchForm
@@ -14,23 +14,31 @@ class SearchView(FormView):
         search_text = form.data['search_query']
         return HttpResponseRedirect(reverse('results', args=(search_text,)))
 
-def results(request, search_text):
-    #The api key is loaded in from environment vaiables
-    api_key = environ['GOOGLE_API_KEY']
-    service = build('books', 'v1', developerKey=api_key)
-    book_request = service.volumes().list(source='public', q=search_text)
-    response = book_request.execute()
+class ResultsView(ListView):
+    template_name = 'results.html'
 
-    books_found = response.get('totalItems', 0)
-    book_list = response.get('items', [])
+    books_found = 0
+    book_list = 0
+    search_text = ''
+    form = SearchForm()
 
-    query = SearchForm()
+    def get_queryset(self):
+        api_key = environ['GOOGLE_API_KEY']
+        service = build('books', 'v1', developerKey=api_key)
+        self.search_text = self.kwargs['search_text']
+        book_request = service.volumes().list(source='public', q=self.search_text)
+        response = book_request.execute()
 
-    context = {
-        'search_text' : search_text,
-        'books_found' : books_found,
-        'book_list' : book_list,
-        'query' : query,
-    }
+        self.books_found = response.get('totalItems', 0)
+        self.book_list = response.get('items', [])
+        return self.book_list
 
-    return render(request, 'results.html', context=context)
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+
+        context['search_text'] = self.search_text
+        context['books_found'] = self.books_found
+        context['book_list'] = self.book_list
+        context['form'] = self.form
+        return context
