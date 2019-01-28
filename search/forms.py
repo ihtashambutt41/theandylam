727ebedbd1fb1cfaa773b9d12fc4ab4eaff84ecc
Andy Lam <theandylam@gmail.com>
2019-01-27 21:51:57 -0800

    Added Bootstrap CSS to both search and results pages

--- search/forms.py
+++ search/forms.py
@@ -1,4 +1,11 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    search_query = forms.CharField(max_length=100, label='Search for a book')
+    search_query = forms.CharField(
+                        max_length=100,
+                        label='',
+                        widget=forms.TextInput(attrs={'class':'form-control',
+                                                'placeholder':'Search for...',
+                                                }
+                        ),
+                    )
--- search/views.py
+++ search/views.py
@@ -10,6 +10,7 @@ class SearchView(FormView):
     template_name = 'search.html'
     form_class = SearchForm
 
+    # Runs this when a valid form input is received
     def form_valid(self, form):
         search_text = form.data['search_query']
         return HttpResponseRedirect(reverse('results', args=(search_text,)))
@@ -18,17 +19,23 @@ class ResultsView(ListView):
     template_name = 'results.html'
 
     books_found = 0
-    book_list = 0
+    book_list = []
     search_text = ''
+
+    # Needed for the search box at the bottom of results page
     form = SearchForm()
 
     def get_queryset(self):
+        # Get API key stored in an environment variable
         api_key = environ['GOOGLE_API_KEY']
+
+        # Querying the Google Books API with the users search text
         service = build('books', 'v1', developerKey=api_key)
         self.search_text = self.kwargs['search_text']
         book_request = service.volumes().list(source='public', q=self.search_text)
         response = book_request.execute()
 
+        # Storing the response from the API
         self.books_found = response.get('totalItems', 0)
         self.book_list = response.get('items', [])
         return self.book_list
@@ -37,6 +44,7 @@ class ResultsView(ListView):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
 
+        # Adding the search results to the context list
         context['search_text'] = self.search_text
         context['books_found'] = self.books_found
         context['book_list'] = self.book_list
